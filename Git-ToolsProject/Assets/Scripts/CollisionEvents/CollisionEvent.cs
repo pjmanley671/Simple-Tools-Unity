d134d0a115aff8eb1eb5a9321f5003c6b1ff9a6c
pjmanley <pjmanley671@gmail.com>
2019-03-18 00:44:10 -0500

    Creating DestroyThisEvent script and DestroyThis asset and made and added folders for organization of future additions.

--- Git-ToolsProject/Assets/Scripts/CollisionEvents/CollisionEvent.cs
+++ Git-ToolsProject/Assets/Scripts/CollisionEvents/CollisionEvent.cs
@@ -4,11 +4,11 @@
 public class CollisionEvent : ScriptableObject
 {
     public virtual void CustomEnter(Collision collision)
-    { Debug.Log("CustomEnter"); }
+    { Debug.Log("CustomEnter - Collision"); }
 
     public virtual void CustomStay(Collision collision)
-    { Debug.Log("CustomStay"); }
+    { Debug.Log("CustomStay - Collision"); }
 
     public virtual void CustomExit(Collision collision)
-    { Debug.Log("CustomExit"); }
+    { Debug.Log("CustomExit - Collision"); }
 }
---
+++ Git-ToolsProject/Assets/Scripts/CollisionEvents/DestroyThisEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu()]
+public class DestroyThisEvent : CollisionEvent
+{
+    public override void CustomEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log(collision.collider.gameObject.name);
+            Destroy(collision.gameObject);
+        }
+    }
+}
